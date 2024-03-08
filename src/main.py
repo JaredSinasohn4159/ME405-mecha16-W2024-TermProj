7e34e34be82b154c0583754727df87705aec801d
Jared Sinasohn <81637028+JaredSinasohn4159@users.noreply.github.com>
2024-03-07 18:29:26 -0800

    added most of the files to run the turret, not working yet

---
+++ src/main.py
@@ -0,0 +1,370 @@
+# YAW
+# EN_PIN = G12
+
+# Timer 1
+# IN_PIN_1 = E9
+# IN_PIN_2 = E11
+
+# Timer 3
+# Encoder_A = A6
+# Encoder_B = B5
+
+# PITCH
+# EN_PIN = G14
+
+# Timer 4
+# IN_PIN_1 = B6
+# IN_PIN_2 = B7
+
+# Timer 2
+# Encoder_A = A15
+# Encoder_B = B3
+
+# TRIGGER
+# Timer 15
+# Signal_pin = E6
+
+"""!
+@file main.py
+    This file modifies the basic_tasks.py file created by Dr. Ridgley and implements a co-tasking
+    system to control the position of two motors at the same time.
+
+@author JR Ridgely
+@author Sydney Ulvick
+@author Jared Sinasohn
+@date   2021-Dec-15 JRR Created from the remains of previous example
+@copyright (c) 2015-2021 by JR Ridgely and released under the GNU
+    Public License, Version 2. 
+"""
+import utime
+import gc
+import pyb
+import cotask
+import task_share
+from motor_drivers.encoder_reader import Encoder
+from motor_drivers.motor_driver import MotorDriver
+from motor_drivers.controller import CLController
+from mlx_cam import MLX_Cam as Cam
+from ulab import numpy as np
+from machine import Pin, I2C
+
+def get_conversion_factor(belt_ratio):
+    """!
+    This function calculates the conversion of the encoder ticks to degrees of the output axis of the motor.
+    @param belt_ratio - the ratio of the belt, output/input
+    """
+    ticks_per_rev = 256 # ticks per rev of the encoder
+    motor_gearbox = 16  # ratio of the gearboxes on the ametek pittman motor
+    counts_per_tick = 4 # number of edge counts per encoder tick
+    rev_per_deg = 1/360 # conversion from revolutions to degrees
+    mech_advantage = motor_gearbox*belt_ratio # mechanical advantage of the whole motor-gearbox setup
+    co_fac = counts_per_tick*ticks_per_rev*rev_per_deg*mech_advantage # the conversion factor in counts/degree
+    return co_fac
+
+def camera_handler_fun():
+    """!
+    This function implements the camera handler task and its finite state machine
+    """
+    t1state = 0
+    image = None
+    im_arr = None
+    camera = None
+    regions = None
+    target = None
+    if t1state == 0:
+        i2c_bus = I2C(1)
+        i2c_address = 0x33
+        camera = Cam(i2c_bus)
+        camera._camera.refresh_rate = 10.0
+        t1state = 1
+        yield t1state
+    else:
+        raise ValueError(f"Invalid State in Task 2.  Current state is {t2state}")
+    while True:
+        if t1state == 1:
+            while not image:
+                image = camera.get_image_nonblocking()
+                yield t1state
+            im_arr = get_array(image)
+            t1state = 2
+            yield t1state
+        elif t1state == 2:
+            #im_arr = medfilt2(im_arr)
+            t1state = 3
+            yield t1state
+        elif t1state == 3:
+            #regions = regionprops(im_arr)
+            #target = max(regions.size)
+            t1state = 4
+            yield t1state
+        elif t1state == 4:
+            #pitch_angle, yaw_angle = get_camera_angles()
+            yaw_angle = 0
+            pitch_angle = 0
+            yaw_motor_setpoint.put(yaw_angle)
+            pitch_motor_setpoint.put(pitch_angle)
+            
+            
+            
+            
+
+def yaw_motor_fun():
+    """!
+    This function controls the yaw motor as part of the second task
+    It runs the motor from 0 degrees to 180 degrees using a proportional controller
+    """
+    t2state = 0
+    yaw_err_list = []
+    yaw_motor_threshold = 1
+    if t2state == 0:
+        # define the encoder conversion factor for the 
+        co_fac1 = get_conversion_factor(1)
+        # motor setup
+        en_pin =  pyb.Pin(machine.Pin.cpu.G14, mode = pyb.Pin.OPEN_DRAIN, pull = pyb.Pin.PULL_UP, value=1)
+        in1pin = pyb.Pin(machine.Pin.cpu.E11, pyb.Pin.OUT_PP)
+        in2pin = pyb.Pin(machine.Pin.cpu.E14, pyb.Pin.OUT_PP)
+        timer = pyb.Timer(1, freq=20000) #setting frequency for motor 
+        motor = MotorDriver(en_pin,in1pin,in2pin,timer) #create motor object
+        motor.set_duty_cycle(0)
+        # encoder setup
+        # create the pin object to read encoder channel A
+        pin1 = pyb.Pin(pyb.Pin.cpu.C6, pyb.Pin.IN)
+        # create the pin object to read encoder channel B
+        pin2 = pyb.Pin(pyb.Pin.cpu.C7, pyb.Pin.IN)
+        # create the timer object.  For C6 and C7 use timer 8,
+        # set the prescaler to zero and the period to the max 16bit number
+        timer = pyb.Timer(8, prescaler = 0, period = 65535)
+        # create the encoder object
+        encoder = Encoder(pin1, pin2, timer, conversion_factor = co_fac1)
+        encoder.set_pos(180)
+        # create controller object
+        con = CLController(1.5, 0.1, 1, 180)
+        t2state = 1
+        yield t2state
+    else:
+        raise ValueError(f"Invalid State in Task 2.  Current state is {t2state}")
+    while True:
+        if t2state == 1:
+            motor.set_duty_cycle(0)
+            if run_motors.get() != 0:
+                t2state = 2
+            yield t2state
+        elif t2state == 2:
+            y_sp = yaw_motor_setpoint.get()
+            con.set_setpoint(y_sp)
+            encoder_angle = encoder.read()
+            yaw_err = y_sp-encoder_angle
+            yaw_err_list.append(yaw_err)
+            if len(yaw_err_list >= 5):
+                avg_yaw_err = sum(yaw_err_list)/5
+                if avg_yaw_err<yaw_motor_threshold:
+                    yaw_motor_done.put(1)
+                yaw_err_list.pop(0)
+            eff = con.run(encoder_angle)
+            motor.set_duty_cycle(eff)
+            yield t2state
+        else:
+            raise ValueError(f"Invalid State in Task 2.  Current state is {t2state}")
+
+
+def pitch_motor_fun():
+    """!
+    This function controls the yaw motor as part of the second task
+    It runs the motor from 0 degrees to 180 degrees using a proportional controller
+    """
+    t3state = 0
+    pitch_err_list = []
+    pitch_motor_threshold = 1
+    if t3state == 0:
+        # define the encoder conversion factor for the 
+        co_fac2 = get_conversion_factor(2)
+        # motor setup
+        en_pin =  pyb.Pin(pyb.Pin.cpu.C2, mode = pyb.Pin.OPEN_DRAIN, pull = pyb.Pin.PULL_UP, value=1)
+        in1pin = pyb.Pin(pyb.Pin.cpu.A3, pyb.Pin.OUT_PP)
+        in2pin = pyb.Pin(pyb.Pin.cpu.C0, pyb.Pin.OUT_PP)
+        timer = pyb.Timer(5, freq=20000) #setting frequency for motor 
+        motor = MotorDriver(en_pin,in1pin,in2pin,timer) #create motor object
+        motor.set_duty_cycle(0)
+        # encoder setup
+        # create the pin object to read encoder channel A
+        pin1 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+        # create the pin object to read encoder channel B
+        pin2 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
+        # create the timer object.  For C6 and C7 use timer 8,
+        # set the prescaler to zero and the period to the max 16bit number
+        timer = pyb.Timer(4, prescaler = 0, period = 65535)
+        # create the encoder object
+        encoder = Encoder(pin1, pin2, timer, conversion_factor = co_fac2)
+        encoder.set_pos(30)
+        # create controller object
+        con = CLController(1.5, 0.1, 1, 180)
+        t3state = 1
+        yield t3state
+    else:
+        raise ValueError(f"Invalid State in Task 3.  Current state is {t3state}")
+    while True:
+        if t3state == 1:
+            motor.set_duty_cycle(0)
+            if run_motors.get() != 0:
+                t3state = 2
+            yield t3state
+        elif t3state == 2:
+            p_sp = pitch_motor_setpoint.get()
+            con.set_setpoint(p_sp)
+            encoder_angle = encoder.read()
+            pitch_err = p_sp-encoder_angle
+            pitch_err_list.append(pitch_err)
+            if len(pitch_err_list >= 5):
+                avg_pitch_err = sum(pitch_err_list)/5
+                if avg_err<pitch_motor_threshold:
+                    pitch_motor_done.put(1)
+                pitch_err_list.pop(0)
+            eff = con.run(encoder_angle)
+            motor.set_duty_cycle(eff)
+            yield t3state
+        else:
+            raise ValueError(f"Invalid State in Task 3.  Current state is {t3state}")
+
+def trigger_fun():
+    """!
+    This function controls the yaw motor as part of the second task
+    It runs the motor from 0 degrees to 180 degrees using a proportional controller
+    """
+    t3state = 0
+    pitch_err_list = []
+    pitch_motor_threshold = 1
+    if t3state == 0:
+        # define the encoder conversion factor for the 
+        co_fac2 = get_conversion_factor(2)
+        # motor setup
+        en_pin =  pyb.Pin(pyb.Pin.board.PC1, mode = pyb.Pin.OPEN_DRAIN, pull = pyb.Pin.PULL_UP, value=1)
+        in1pin = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+        in2pin = pyb.Pin(pyb.Pin.board.PA1, pyb.Pin.OUT_PP)
+        timer = pyb.Timer(5, freq=20000) #setting frequency for motor 
+        motor = MotorDriver(en_pin,in1pin,in2pin,timer) #create motor object
+        motor.set_duty_cycle(0)
+        # encoder setup
+        # create the pin object to read encoder channel A
+        pin1 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+        # create the pin object to read encoder channel B
+        pin2 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
+        # create the timer object.  For C6 and C7 use timer 8,
+        # set the prescaler to zero and the period to the max 16bit number
+        timer = pyb.Timer(4, prescaler = 0, period = 65535)
+        # create the encoder object
+        encoder = Encoder(pin1, pin2, timer, conversion_factor = co_fac2)
+        encoder.set_pos(30)
+        # create controller object
+        con = CLController(1.5, 0.1, 1, 180)
+        t3state = 1
+        yield t3state
+    else:
+        raise ValueError(f"Invalid State in Task 3.  Current state is {t3state}")
+    while True:
+        if t3state == 1:
+            motor.set_duty_cycle(0)
+            if run_motors.get() != 0:
+                t3state = 2
+            yield t3state
+        elif t3state == 2:
+            p_sp = pitch_motor_setpoint.get()
+            con.set_setpoint(p_sp)
+            encoder_angle = encoder.read()
+            pitch_err = p_sp-encoder_angle
+            pitch_err_list.append(pitch_err)
+            if len(pitch_err_list >= 5):
+                avg_pitch_err = sum(pitch_err_list)/5
+                if avg_err<pitch_motor_threshold:
+                    pitch_motor_done.put(1)
+                pitch_err_list.pop(0)
+            eff = con.run(encoder_angle)
+            motor.set_duty_cycle(eff)
+            yield t3state
+        else:
+            raise ValueError(f"Invalid State in Task 3.  Current state is {t3state}")
+
+def timing_handler_fun():
+    t5state = 0
+    tstart = utime.ticks_ms()
+    tend = tstart+5000
+    if t5state == 0:
+        t5state = 1
+        yield t5state
+    while True:
+        if t5state == 1:
+            curr_time = utime.ticks_ms()
+            if curr_time >= tend:
+                run_motors.put(1)
+            t5state = 2
+            yield t5state
+        elif t5state == 2:
+            t5state = 2
+            yield t5state
+        else:
+            raise ValueError(f"Invalid State in Task 5.  Current state is {t5state}")
+
+#def serial_communication():
+#    """!
+#    This function controls the serial communication between the microcontroller
+#    and the computer to plot responses if necessary.  It prints the list of values
+#    one value at a time.
+#    """
+#    while True:
+#        if not time1.full() or not pos1.full():
+#            yield 0
+#        else:
+#            while time1.any() and pos1.any():
+#                print(f"{time1.get()}, {pos1.get()}")
+#                yield 0
+# This code creates a share, a queue, and two tasks, then starts the tasks. The
+# tasks run until somebody presses ENTER, at which time the scheduler stops and
+# printouts show diagnostic information about the tasks, share, and queue.
+if __name__ == "__main__":
+    print("Testing ME405 stuff in cotask.py and task_share.py\r\n"
+          "Press Ctrl-C to stop and show diagnostics.")
+    #set angle values (degrees) for motor 1 and motor 2, labeled accordingly
+    # Create the tasks. If trace is enabled for any task, memory will be
+    # allocated for state transition tracing, and the application will run out
+    # of memory after a while and quit. Therefore, use tracing only for 
+    # debugging and set trace to False when it's not needed
+    run_motors = task_share.Share("B", name="Run the motors boolean")
+    yaw_motor_done = task_share.Share("B", name="Yaw motor reached location boolean")
+    pitch_motor_done = task_share.Share("B", name="Pitch motor reached location boolean")
+    yaw_motor_setpoint = task_share.Share("f", name="Yaw motor setpoint")
+    pitch_motor_setpoint = task_share.Share("f", name="Pitch motor setpoint")
+    run_motors.put(0)
+    yaw_motor_done.put(0)
+    pitch_motor_done.put(0)
+    task1 = cotask.Task(camera_handler_fun, name="Task 1: Camera Handler", priority=5,period=15,
+                        profile=True, trace=False)
+    task2 = cotask.Task(yaw_motor_fun, name="Task 2: Yaw Motor Handler", priority=9,period=15,
+                        profile=True, trace=False)
+    task3 = cotask.Task(pitch_motor_fun, name="Task 3: Pitch Motor Handler", priority=9,period=15,
+                        profile=True, trace=False)
+    task4 = cotask.Task(trigger_fun, name="Task 4: Trigger handler", priority=10,period=15,
+                        profile=True, trace=False)
+#    task3 = cotask.Task(serial_communication, name="Task 3", priority=3,period=10,
+#                        profile=True, trace=False)
+    task5 = cotask.Task(timing_handler_fun, name="Task 4: Trigger handler", priority=10,period=15,
+                        profile=True, trace=False)  
+    # adding the tasks to task list, commenting out task3, used for acquiring data for plotting
+    cotask.task_list.append(task1) #add tasks to scheduler list
+    cotask.task_list.append(task2) #add tasks to scheduler list
+    #cotask.task_list.append(task3)
+    
+    
+    # Run the memory garbage collector to ensure memory is as defragmented as
+    # possible before the real-time scheduler is started
+    gc.collect()
+    # Run the scheduler with the chosen scheduling algorithm. Quit if ^C pressed
+    while True:
+        try:
+            cotask.task_list.pri_sched()
+        except KeyboardInterrupt:
+            break
+
+    # Print a table of task data and a table of shared information data
+    print('\n' + str (cotask.task_list))
+    print(task_share.show_all())
+    print(task1.get_trace())
+    print('')
---
+++ src/mlx_cam.py
@@ -0,0 +1,228 @@
+## @file mlx_cam.py
+# 
+#  RAW VERSION
+#  This version uses a stripped down MLX90640 driver which produces only raw
+#  data, not calibrated data, in order to save memory.
+# 
+#  This file contains a wrapper that facilitates the use of a Melexis MLX90640
+#  thermal infrared camera for general use. The wrapper contains a class MLX_Cam
+#  whose use is greatly simplified in comparison to that of the base class,
+#  @c class @c MLX90640, by mwerezak, who has a cool fox avatar, at
+#  @c https://github.com/mwerezak/micropython-mlx90640
+# 
+#  To use this code, upload the directory @c mlx90640 from mwerezak with all
+#  its contents to the root directory of your MicroPython device, then copy
+#  this file to the root directory of the MicroPython device.
+# 
+#  There's some test code at the bottom of this file which serves as a
+#  beginning example.
+# 
+#  @author mwerezak Original files, Summer 2022
+#  @author JR Ridgely Added simplified wrapper class @c MLX_Cam, January 2023
+#  @copyright (c) 2022-2023 by the authors and released under the GNU Public
+#      License, version 3.
+
+import utime as time
+from machine import Pin, I2C
+from mlx90640 import MLX90640
+from mlx90640.calibration import NUM_ROWS, NUM_COLS, IMAGE_SIZE, TEMP_K
+from mlx90640.image import ChessPattern, InterleavedPattern
+from ulab import numpy as np
+
+
+## @brief   Class which wraps an MLX90640 thermal infrared camera driver to
+#           make it easier to grab and use an image. 
+#  @details This image is in "raw" mode, meaning it has not been calibrated
+#           (which takes lots of time and memory) and only gives relative IR
+#           emission seen by pixels, not estimates of the temperatures.
+class MLX_Cam:
+
+    ## @brief   Set up an MLX90640 camera.
+    #  @param   i2c An I2C bus which has been set up to talk to the camera;
+    #           this must be a bus object which has already been set up
+    #  @param   address The address of the camera on the I2C bus (default 0x33)
+    #  @param   pattern The way frames are interleaved, as we read only half
+    #           the pixels at a time (default ChessPattern)
+    #  @param   width The width of the image in pixels; leave it at default
+    #  @param   height The height of the image in pixels; leave it at default
+    def __init__(self, i2c, address=0x33, pattern=ChessPattern,
+                 width=NUM_COLS, height=NUM_ROWS):
+
+        ## The I2C bus to which the camera is attached
+        self._i2c = i2c
+        ## The address of the camera on the I2C bus
+        self._addr = address
+        ## The pattern for reading the camera, usually ChessPattern
+        self._pattern = pattern
+        ## The width of the image in pixels, which should be 32
+        self._width = width
+        ## The height of the image in pixels, which should be 24
+        self._height = height
+        ## Tracks whether an image is currently being retrieved
+        self._getting_image = False
+        ## Which subpage (checkerboard half) of the image is being retrieved
+        self._subpage = 0
+
+        # The MLX90640 object that does the work
+        self._camera = MLX90640(i2c, address)
+        self._camera.set_pattern(pattern)
+        self._camera.setup()
+
+        ## A local reference to the image object within the camera driver
+        self._image = self._camera.raw
+        
+    def get_array(self, array, limits=None):
+        if limits and len(limits) == 2:
+            scale = (limits[1] - limits[0]) / (max(array) - min(array))
+            offset = limits[0] - min(array)
+        else:
+            offset = 0.0
+            scale = 1.0
+        arr = np.zeros((self._height, self._width), dtype=np.uint8)
+        for row in range(self._height):
+            for col in range(self._width):
+                pix = int((array[row * self._width + (self._width - col - 1)]
+                          + offset) * scale)
+                arr[row,col] = pix
+        return arr
+    
+    def get_csv(self, array, limits=None):
+
+        if limits and len(limits) == 2:
+            scale = (limits[1] - limits[0]) / (max(array) - min(array))
+            offset = limits[0] - min(array)
+        else:
+            offset = 0.0
+            scale = 1.0
+        for row in range(self._height):
+            line = ""
+            for col in range(self._width):
+                pix = int((array[row * self._width + (self._width - col - 1)]
+                          + offset) * scale)
+                if col:
+                    line += ","
+                line += f"{pix}"
+            yield line
+        return
+
+
+    ## @brief   Get an image from an MLX90640 camera in a non-blocking way.
+    #  @details This function is to be called repeatedly; it will return @c None
+    #           until a complete image has been retrieved (this takes around a
+    #           quarter to half second) and will then return the image.
+    #
+    #      @b Example: This code would be inside a task function which yields 
+    #      repeatedly as long as there isn't a complete image available.
+    #      @code
+    #      image = None
+    #      while not image:
+    #          image = camera.get_image_nonblocking()
+    #          yield(state)
+    #      @endcode
+    #
+    def get_image_nonblocking(self):
+
+        # If this is the first recent call, begin the process
+        if not self._getting_image:
+            self._subpage = 0
+            self._getting_image = True
+        
+        # Read whichever subpage needs to be read, or wait until data is ready
+        if not self._camera.has_data:
+            return None
+        
+        image = self._camera.read_image(self._subpage)
+        
+        # If we just got subpage zero, we need to come back and get subpage 1;
+        # if we just got subpage 1, we're done
+        if self._subpage == 0:
+            self._subpage = 1
+            return None
+        else:
+            self._getting_image = False
+            return image
+
+
+## This test function sets up the sensor, then grabs and shows an image in a
+#  terminal every few seconds. By default it shows ASCII art, but it can be
+#  set to show better looking grayscale images in some terminal programs such
+#  as PuTTY. Unfortunately Thonny's terminal won't show the nice grayscale. 
+def test_MLX_cam():
+
+    import gc
+
+    # The following import is only used to check if we have an STM32 board such
+    # as a Pyboard or Nucleo; if not, use a different library
+    try:
+        from pyb import info
+
+    # Oops, it's not an STM32; assume generic machine.I2C for ESP32 and others
+    except ImportError:
+        # For ESP32 38-pin cheapo board from NodeMCU, KeeYees, etc.
+        i2c_bus = I2C(1, scl=Pin(22), sda=Pin(21))
+
+    # OK, we do have an STM32, so just use the default pin assignments for I2C1
+    else:
+        i2c_bus = I2C(1)
+
+    print("MXL90640 Easy(ish) Driver Test")
+
+    # Select MLX90640 camera I2C address, normally 0x33, and check the bus
+    i2c_address = 0x33
+    scanhex = [f"0x{addr:X}" for addr in i2c_bus.scan()]
+    print(f"I2C Scan: {scanhex}")
+
+    # Create the camera object and set it up in default mode
+    camera = MLX_Cam(i2c_bus)
+    print(f"Current refresh rate: {camera._camera.refresh_rate}")
+    camera._camera.refresh_rate = 10.0
+    print(f"Refresh rate is now:  {camera._camera.refresh_rate}")
+
+    while True:
+        try:
+            input()
+            # Get and image and see how long it takes to grab that image
+            print("Click.", end='')
+            begintime = time.ticks_ms()
+            image = camera.get_image()
+
+            # Keep trying to get an image; this could be done in a task, with
+            # the task yielding repeatedly until an image is available
+            image = None
+            while not image:
+                image = camera.get_image_nonblocking()
+                #time.sleep_ms(50)
+            print(f" {time.ticks_diff(time.ticks_ms(), begintime)} ms")
+
+            # Can show image.v_ir, image.alpha, or image.buf; image.v_ir best?
+            # Display pixellated grayscale or numbers in CSV format; the CSV
+            # could also be written to a file. Spreadsheets, Matlab(tm), or
+            # CPython can read CSV and make a decent false-color heat plot.
+            show_image = False
+            show_csv = True
+            time.sleep_ms(2000)
+            #print(camera.get_array(image, limits=(0, 255)))
+            if show_image:
+                camera.ascii_image(image)
+            elif show_csv:
+                for line in camera.get_csv(image, limits=(0, 255)):
+                    print(line)
+                pass
+            else:
+                camera.ascii_art(image)
+            gc.collect()
+            print(f"Memory: {gc.mem_free()} B free")
+            #time.sleep_ms(3141)
+
+        except KeyboardInterrupt:
+            break
+
+    print ("Done.")
+
+
+if __name__ == "__main__":
+
+    test_MLX_cam()
+
+
+
